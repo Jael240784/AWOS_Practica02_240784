c2543ad2382151a6ba421cbe5e32ad8d35c7b751
Jael240784 <240784@utxicotepec.edu.mx>
2026-01-07 09:49:32 -0600

    practica 02
    add: se agrego el archivo del ejercicio 01 - declaracion de variables, aun sin terminar

---
+++ js/ejercicio01.js
@@ -0,0 +1,15 @@
+// 1. Declaración de variables utilizando el prefijo VAR
+var miNombre = "Jael A.";
+var misApellidos;
+
+//Utilizaremos la función console.log para imprimir el valor actual de la variable
+console.log("El valor de la variable miNombre es:", miNombre);
+console.log("El valor de la variable misApellidos es:", misApellidos);
+//Esto arroja un undefined dado que la variable no ha sido inicializada, es decir, no se le ha asignado un valor almacenado.
+
+misApellidos = "Gonzalez Cruz";
+//En este momento la bariable ya tiene un valor almacenado por que al imprimirlo deberá mostrar el valor asignado.
+console.log("El valor de la variable misApellidos es:", misApellidos);
+
+miNombre= " Jael A.";
+console.log("Hola, ", miNombre, misApellidos);
